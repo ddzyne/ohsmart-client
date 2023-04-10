7deee8b6eb1246fed26040cdfef5c96d60dbe5f6
Daan <daancjanssen@users.noreply.github.com>
2023-04-10 11:27:18 +0200

    wip

---
+++ src/features/metadata/metadataSlice.ts
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice, PayloadAction, current } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+import formSections from '../../config/formsections';
+
+// load the imported form and close all accordion panels by default
+const initialState: any = {
+  form: formSections,
+  panel: '',
+}
+
+// recursive function to find this field in the state and update its value
+function formatData(arr: any, payload: any) {
+  arr && arr.forEach((i: any) => {
+    if( i.id === payload.field.id ) {
+      i.value = payload.value;
+      i.touched = true;
+    } else {
+      formatData(i.fields, payload)
+    }
+  });
+}
+
+export const metadataSlice = createSlice({
+  name: 'metadata',
+  initialState,
+  reducers: {
+    // keep track of form state
+    setMetadata: (state, action: PayloadAction<any>) => {
+      formatData(state.form, action.payload)
+    },
+    // keep track of the accordion state
+    setOpenPanel: (state, action: PayloadAction<any>) => {
+      state.panel = action.payload;
+    }
+  }
+});
+
+export const { setMetadata, setOpenPanel } = metadataSlice.actions;
+
+// Select values from state
+export const getMetadata = (state: RootState) => state.metadata.form;
+export const getOpenPanel = (state: RootState) => state.metadata.panel;
+
+export default metadataSlice.reducer;
