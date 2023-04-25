462527c88e931f221839e790d9a7af3585e999d8
Daan <daancjanssen@users.noreply.github.com>
2023-04-25 15:57:24 +0200

    better orcid search

--- src/features/metadata/api/orcid.ts
+++ src/features/metadata/api/orcid.ts
@@ -4,12 +4,16 @@ export const orcidAPI = createApi({
   reducerPath: 'orcid',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://pub.orcid.org/v3.0/expanded-search/' }),
   endpoints: (build) => ({
-    fetchOrcid: build.query<any, string>({
-      query: (content) => ({
-        url: `?q=${content}`,
-        headers: {Accept: "application/vnd.orcid+json"},
-      }),
-      transformResponse: (response: any, meta, arg) => {
+    fetchOrcid: build.query({
+      query: (content) => {
+        // Reformat the input string, replacing all spaces with +AND+ operators (and remove double spaces too)
+        const search = content.replace(/\s+/g,'+AND+');
+        return ({
+          url: `?q=${search}`,
+          headers: {Accept: "application/vnd.orcid+json"},
+        })
+      },
+      transformResponse: (response: any) => {
         // Return an empty array when no results, which is what the Autocomplete field expects
         return response['num-found'] > 0 ? 
           response['expanded-result'].map( (item: any) => ({
--- src/features/metadata/api/ror.ts
+++ src/features/metadata/api/ror.ts
@@ -4,12 +4,12 @@ export const rorAPI = createApi({
   reducerPath: 'ror',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.ror.org' }),
   endpoints: (build) => ({
-    fetchRorByName: build.query<any, string>({
+    fetchRorByName: build.query({
       query: (content) => ({
         url: `organizations?query.advanced=name:${content}`,
         headers: {Accept: "application/json"},
       }),
-      transformResponse: (response: any, meta, arg) => {
+      transformResponse: (response: any) => {
         // Return an empty array when no results, which is what the Autocomplete field expects
         return response.number_of_results > 0 ? 
           response.items.map( (item: any) => ({
